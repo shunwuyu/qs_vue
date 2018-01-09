4a8c301c4efc001f5d70a3bad24b2f066fd3ce5a
shunwuyu <shunwu2001@163.com>
2018-01-09 11:42:44 +0800

    transition

--- vue-router/basic/src/main.js
+++ vue-router/basic/src/main.js
@@ -1,32 +1,73 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Post from './Post.vue'
 
 Vue.use(VueRouter)
 
-const Home = { template: '<div>home</div>' }
+const Home = {
+  template: `
+    <div class="home">
+      <h2>Home</h2>
+      <p>hello</p>
+    </div>
+  `
+}
+
+const Parent = {
+  data () {
+    return {
+      transitionName: 'slide-left'
+    }
+  },
+  beforeRouteUpdate(to, from, next) {
+    const toDepth = to.path.split('/').length;
+    console.log(toDepth);
+    const fromDepth = from.path.split('/').length;
+    console.log(fromDepth);
+    this.transitionName = toDepth < fromDepth ? 'slide-right' : 'slide-left'
+    next()
+  },
+  template: `
+    <div class="parent">
+      <h2>Parent</h2>
+      <transition :name="transitionName">
+        <router-view class="child-view"></router-view>
+      </transition>
+    </div>
+  `
+}
+
+const Default = { template: '<div class="default">default</div>' }
+const Foo = { template: '<div class="foo">foo</div>' }
+const Bar = { template: '<div class="bar">bar</div>' }
 
 const router = new VueRouter({
   mode: 'history',
   base: __dirname,
   routes: [
     { path: '/', component: Home },
-    { path: '/post/:id', component: Post }
+    { path: '/parent', component: Parent,
+      children: [
+        { path: '', component: Default },
+        { path: 'foo', component: Foo },
+        { path: 'bar', component: Bar }
+      ]},
   ]
-});
+})
 
 new Vue({
   router,
   template: `
     <div id="app">
-      <h1>Data Fetching</h1>
+      <h1>Transitions</h1>
       <ul>
         <li><router-link to="/">/</router-link></li>
-        <li><router-link to="/post/1">/post/1</router-link></li>
-        <li><router-link to="/post/2">/post/2</router-link></li>
-        <li><router-link to="/post/3">/post/3</router-link></li>
+        <li><router-link to="/parent">/parent</router-link></li>
+        <li><router-link to="/parent/foo">/parent/foo</router-link></li>
+        <li><router-link to="/parent/bar">/parent/bar</router-link></li>
       </ul>
-      <router-view class="view"></router-view>
+      <transition name="fade" mode="out-in">
+        <router-view class="view"></router-view>
+      </transition>
     </div>
   `
 }).$mount('#app');
