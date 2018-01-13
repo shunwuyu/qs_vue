8b24bbbad35bc5ef39803f6a74718fa0e17b86e7
shunwuyu <shunwu2001@163.com>
2018-01-13 19:36:06 +0800

    element-ui源码

---
+++ router-post/src/components/ElBadge.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="el-badge">
+    <slot></slot>
+    <transition 
+    name="el-zoom-in-center">
+      <sup
+      v-show="!hidden&&(content||isDot)" 
+      class="el-badge__content"
+      :class="{'is-fixed': true,
+       'is-dot': isDot}"
+      v-text="content">
+      </sup>
+    </transition>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'ElBadge',
+    props: {
+      value:{},
+      max: Number,
+      isDot: Boolean,
+      hidden: Boolean
+    },
+    computed: {
+      content() {
+        if (this.isDot) return;
+
+        const value = this.value
+        const max = this.max
+        
+        if (typeof value === 'number' && 
+        typeof max === 'number') {
+          return max < value ? `${max}+` : value;
+        }
+      }
+    }
+  }
+</script>
+<style>
+
+</style>
